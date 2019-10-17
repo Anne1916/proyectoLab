11e24ed0d40250275822656a388b8a1f53dd56b2
Francisco Pantoja <fcopantoja@gmail.com>
2019-10-16 20:09:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ let nombre = prompt("Juego de Trivia, ingresa tu nombre")
 console.log("Hola" + nombre)
 alert("Hola " + nombre + " bienvenida a la Trivia, comencemos.")
 
+let aciertos = 0;
+let errores = 0;
 
-let aciertos = 0
-let errores = 0
+const RESPUESTAS_CORRECTAS = {
+    'respuesta1': '2'
+};
 
 const revisar = () => {
-    if(document.getElementById("correcta").checked == true){
-      aciertos ++
-      
-}else{
-    errores ++ 
-}
+    for (let key in RESPUESTAS_CORRECTAS) {
+        const respuesta = document.querySelector(`input[name="${key}"]:checked`).value;
 
-document.getElementById("resultados").innerHTML = aciertos
-document.write(errores, aciertos)
-}
+        if (respuesta === RESPUESTAS_CORRECTAS[key]) {
+            aciertos += 1;
+        }
+    }
+
+    document.getElementById("resultados").innerHTML = aciertos;
+};
